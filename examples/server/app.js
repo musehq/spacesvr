b7c4145a46634b650ddc57c55e1093508a4c47ff
Vishaal <vschittur@gmail.com>
2020-12-26 00:51:31 -0500

    peer discovery wip

--- examples/server/app.js
+++ examples/server/app.js
@@ -1,27 +1,40 @@
 const http = require("http");
 const express = require("express");
+const cors = require("cors");
 const { ExpressPeerServer } = require("peer");
 
 process.title = "spaces-server";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// app.use(cors({origin: true, credentials: true}));
 app.set("port", PORT);
 app.get("/", (req, res, next) => res.send("Lorem ipsum"));
 
 const httpServer = http.createServer(app);
 const peerServer = ExpressPeerServer(httpServer, {
+  allow_discovery: true,
   debug: true,
 });
+
 app.use("/signal", peerServer);
+// var connections = [];
 
 peerServer.on("connection", (client) => {
   console.log("Client connected ", client.id);
+  // var idx = connected.indexOf(client.id);
+  // if (idx != -1) { connected.push(client.id) }
 });
 
 peerServer.on("disconnect", (client) => {
   console.log("Client disconnected ", client.id);
+  // var idx = connected.indexOf(client.id);
+  // if (idx != -1) { connected.splice(idx, 1) }
 });
+
+// app.get('/listPeers', (req, res) => {
+//   return res.json(connected);
+// });
 
 httpServer.listen(PORT);
 console.log("Peer server running @ http://localhost: ", PORT);
--- src/modifiers/Networked.tsx
+++ src/modifiers/Networked.tsx
@@ -8,10 +8,51 @@ type NetworkedProps = {
 };
 
 export const Networked = (props: NetworkedProps) => {
+  let peerId: string;
+  let conn: Peer.DataConnection;
+
   const peer = new Peer({
     host: props.host,
     port: props.port,
     path: props.path,
+  });
+
+  peer.on("open", (id) => {
+    peerId = id;
+    // Connect to other peers
+    // fetch('http://localhost:3001/listPeers')
+    // .then(res => res.json())
+    // .then(data => { for (let pid of data) {
+    // 	if (pid != peerId) {
+    // 		conn = peer.connect(pid)
+    // 		conn.send('hello');
+    // 	}
+    // }})
+    // .catch(err => console.log(err));
+    peer.listAllPeers((peers) => {
+      console.log(peers);
+      for (const p of peers) {
+        if (p != peerId) {
+          conn = peer.connect(p);
+
+          conn.on("open", () => {
+            conn.send("Hello from " + peerId);
+          });
+
+          conn.on("data", (data) => {
+            console.log(data);
+          });
+        }
+      }
+    });
+  });
+
+  peer.on("close", () => {
+    console.log("Closed connection");
+  });
+
+  peer.on("error", (err) => {
+    alert(err);
   });
 
   return null;
