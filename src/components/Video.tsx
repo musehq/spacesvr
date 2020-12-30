e5bfbb44996b11a1a41c052817a714bf8025592d
Alex Shortt <alexander.shortt@gmail.com>
2020-12-30 13:48:46 -0800

    v1.2.15 fixed permissions for video playback, cleanup video on unmount

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { useThree } from "react-three-fiber";
 import Frame from "./misc/Frame";
@@ -21,6 +21,7 @@ export const Video = (props: VideoProps) => {
   const [width, height] = size;
   const listener = useRef<THREE.AudioListener>();
   const [speaker, setSpeaker] = useState<THREE.PositionalAudio>();
+
   const [video] = useState(() => {
     const v = document.createElement("video");
     // @ts-ignore
@@ -29,38 +30,53 @@ export const Video = (props: VideoProps) => {
     v.loop = true;
     v.src = src;
     v.muted = muted ? muted : false;
-    if (v.muted) {
-      v.play();
-    }
     return v;
   });
 
+  const playAudio = useCallback(() => {
+    if (video) {
+      video.play();
+    }
+
+    if (!muted && video && !speaker) {
+      listener.current = new THREE.AudioListener();
+      camera.add(listener.current);
+
+      const speak = new THREE.PositionalAudio(listener.current);
+      speak.setMediaElementSource(video);
+      speak.setRefDistance(0.75);
+      speak.setRolloffFactor(1);
+      speak.setVolume(1);
+      speak.setDirectionalCone(180, 230, 0.1);
+
+      setSpeaker(speak);
+    }
+  }, [video, muted, speaker]);
+
   useEffect(() => {
-    const onClick = () => {
-      if (video) {
-        video.play();
-      }
-
-      if (!muted && video && !speaker) {
-        listener.current = new THREE.AudioListener();
-        camera.add(listener.current);
-
-        const speak = new THREE.PositionalAudio(listener.current);
-        speak.setMediaElementSource(video);
-        speak.setRefDistance(0.75);
-        speak.setRolloffFactor(1);
-        speak.setVolume(1);
-        speak.setDirectionalCone(180, 230, 0.1);
-
-        setSpeaker(speak);
-      }
-    };
-
-    document.addEventListener("click", onClick);
+    playAudio();
+    document.addEventListener("click", playAudio);
     return () => {
-      document.removeEventListener("click", onClick);
+      document.removeEventListener("click", playAudio);
     };
   }, [video, muted, speaker]);
+
+  useEffect(() => {
+    return () => {
+      if (listener.current) {
+        camera.remove(listener.current);
+        listener.current.clear();
+      }
+      if (speaker) {
+        speaker.clear();
+        speaker.disconnect();
+      }
+      if (video) {
+        video.pause();
+        video.remove();
+      }
+    };
+  }, []);
 
   return (
     <group {...props}>
