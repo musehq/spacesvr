8f7fc411215dbe0b2cfc6c9229bc2e6594d190ac
Alex Shortt <alexander.shortt@gmail.com>
2020-12-31 20:25:41 -0800

    fix blurry hdri

--- src/components/HDRI.tsx
+++ src/components/HDRI.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 
 import { useThree } from "react-three-fiber";
-import { PMREMGenerator, UnsignedByteType } from "three";
+import { UnsignedByteType } from "three";
+import * as THREE from "three";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 
 type HDRIProps = {
@@ -13,28 +14,37 @@ export const HDRI = (props: HDRIProps) => {
   const { src, hideBackground } = props;
   const { gl, scene } = useThree();
 
-  // pmrem generator for hdri loading
-  const pmremGenerator = new PMREMGenerator(gl);
-  pmremGenerator.compileEquirectangularShader();
-
   // actual file loader
   const loader = new RGBELoader();
   loader.setDataType(UnsignedByteType);
 
   useEffect(() => {
     loader.load(src, (texture) => {
-      const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+      const opts = {
+        format: THREE.RGBAFormat,
+        generateMipmaps: false,
+        magFilter: THREE.NearestFilter,
+        minFilter: THREE.NearestFilter,
+      };
+      const envMap = new THREE.WebGLCubeRenderTarget(
+        4096,
+        opts
+      ).fromEquirectangularTexture(gl, texture);
 
       // sent envmap onto scene env and background
       if (!hideBackground) {
         scene.background = envMap;
       }
+      // @ts-ignore
       scene.environment = envMap;
 
       texture.dispose();
-      pmremGenerator.dispose();
+
+      return () => {
+        envMap.dispose();
+      };
     });
-  }, [src, scene, loader, pmremGenerator, hideBackground]);
+  }, [src, scene, loader, hideBackground]);
 
   return null;
 };
