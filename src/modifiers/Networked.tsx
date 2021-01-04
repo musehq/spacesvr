91af4de4d9391790ee70191e857a48e2e87b60c2
Vishaal <vschittur@gmail.com>
2021-01-04 15:18:23 -0500

    hoist data conn func

--- src/modifiers/Networked.tsx
+++ src/modifiers/Networked.tsx
@@ -9,17 +9,31 @@ type NetworkedProps = {
 
 export const Networked = (props: NetworkedProps) => {
   let peerId: string;
-  let peerConn: Peer.DataConnection;
+  let dataConn: Peer.DataConnection;
   let socket: WebSocket;
+
+  const handleDataConn = (dataConn: Peer.DataConnection): void => {
+    dataConn.on("open", () => {
+      dataConn.send("Hello from " + peerId);
+    });
+
+    dataConn.on("data", (data) => {
+      console.log(data);
+    });
+
+    dataConn.on("close", () => {
+      console.log("Closed data connection");
+    });
+
+    dataConn.on("error", (err) => {
+      alert(err);
+    });
+  };
 
   const connectPeer = (peer: Peer, newPeer: string): void => {
     if (peerId != newPeer) {
-      peerConn = peer.connect(newPeer);
-
-      peerConn.on("open", () => {
-        peerConn.send("Hello from " + peerId);
-      });
-
+      dataConn = peer.connect(newPeer);
+      handleDataConn(dataConn);
       console.log("Connected with " + newPeer);
     }
   };
@@ -27,7 +41,6 @@ export const Networked = (props: NetworkedProps) => {
   const connectP2P = (peer: Peer): void => {
     peer.listAllPeers((peers) => {
       console.log(peers);
-
       if (peers && peers.length) {
         for (const p of peers) {
           connectPeer(peer, p);
@@ -50,28 +63,28 @@ export const Networked = (props: NetworkedProps) => {
     // Listen for future peers
     socket = new WebSocket("ws://localhost:8080");
 
-    socket.addEventListener("open", (event) => {
+    socket.onopen = (event) => {
       socket.send(peerId);
-    });
+    };
 
-    socket.addEventListener("message", (event) => {
+    socket.onmessage = (event) => {
       connectPeer(peer, event.data);
-    });
+    };
   });
 
   peer.on("connection", (conn) => {
-    // Handle stream
+    // Handle stream, logs hello for now
     conn.on("data", (data) => {
       console.log(data);
     });
 
     conn.on("close", () => {
-      console.log("Closed data connection " + peerId);
+      console.log("Closed peer connection");
     });
   });
 
   peer.on("close", () => {
-    peer.destroy();
+    // peer.destroy();
     console.log("Destroyed " + peerId);
   });
 
