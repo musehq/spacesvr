32ace4fcab4cd4b6796eec5ee9aaec5afa944061
Vishaal <vschittur@gmail.com>
2021-01-08 20:10:36 -0500

    double peer bug fix, sendEvent with connection map

--- examples/src/index.js
+++ examples/src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
