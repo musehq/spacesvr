3e9ce55f8644d05fcc1912432b90c4e2c01fa5ca
Alex Shortt <alexander.shortt@gmail.com>
2021-01-14 21:26:54 -0800

    v1.2.21 useValidBrowser

--- src/core/utils/hooks.ts
+++ src/core/utils/hooks.ts
@@ -128,3 +128,24 @@ export const useControlledProgress = () => {
 
   return skip ? 100 : Math.floor(controlledProgress.current);
 };
+
+/**
+ * Check validity of browser to run 3d experiences,
+ * Automatically blacklists Facebook & Instagram in-app
+ * browsers
+ *
+ * @param keywords
+ */
+export const useValidBrowser = (keywords?: string[]) => {
+  const [valid, setValid] = useState(true);
+
+  const INVALID_KEYWORDS = ["FBAN", "FBAV", "Instagram"].concat(keywords || []);
+
+  useEffect(() => {
+    const ua = navigator.userAgent || navigator.vendor || "";
+    const valid = INVALID_KEYWORDS.filter((val) => ua.includes(val));
+    setValid(valid.length === 0);
+  }, []);
+
+  return valid;
+};
