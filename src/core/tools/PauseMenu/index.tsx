c4c8612312c6777ce71a2912b257a32f9483f341
Alex Shortt <alexander.shortt@gmail.com>
2021-02-06 14:09:04 -0800

    menu state, initial onboard

--- src/core/tools/PauseMenu/index.tsx
+++ src/core/tools/PauseMenu/index.tsx
@@ -6,9 +6,16 @@ import { Tool } from "../../../modifiers/Tool";
 // @ts-ignore
 import { animated, useSpring } from "react-spring/three";
 import { config } from "react-spring";
+import Welcome from "./components/Welcome";
+import { hasOnboarded, setOnboarded } from "./utils/onboard";
+
+export type MenuState = "welcome" | "menu" | "tutorial";
 
 const PauseMenu = () => {
   const [open, setOpen] = useState(true);
+  const [state, setState] = useState<MenuState>(
+    hasOnboarded() ? "menu" : "welcome"
+  );
 
   const onKeyPress = (e: KeyboardEvent) => {
     if (e.key.toLowerCase() === "t") {
@@ -33,7 +40,10 @@ const PauseMenu = () => {
       <Floating height={1}>
         <animated.group scale-x={scale} scale-y={scale} scale-z={scale}>
           <Idea size={2} />
-          <Menu position-x={2} position-y={-2} />
+          {state === "welcome" && (
+            <Welcome position-x={2} setState={setState} />
+          )}
+          {state === "menu" && <Menu position-x={2} />}
         </animated.group>
       </Floating>
     </Tool>
