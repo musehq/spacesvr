b8ccd9ef4520b29465ce1eb2dc457e7b3a0e78d4
Alex Shortt <alexander.shortt@gmail.com>
2021-04-13 00:25:38 -0700

    v1.5.4

--- src/core/overlays/DesktopPause.tsx
+++ src/core/overlays/DesktopPause.tsx
@@ -168,7 +168,7 @@ const DesktopPause = () => {
       <Container paused={paused}>
         <ClickContainer onClick={closeOverlay} />
         <Window>
-          <Version>v1.5.3</Version>
+          <Version>v1.5.4</Version>
           <Instagram
             onClick={() => window.open("https://www.instagram.com/musehq")}
           >
