a431e6b95738a4ada83f3fbbe6200dc989fb2cb3
Alaric Baraou <alaric.baraou@gmail.com>
2021-06-06 14:37:28 +0200

    Multinational Keyboard Support

--- src/core/overlays/DesktopPause.tsx
+++ src/core/overlays/DesktopPause.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { useRef } from "react";
 import { isMobile } from "react-device-detect";
 import { useEnvironment } from "../contexts/environment";
 import { keyframes } from "@emotion/core";
@@ -169,10 +170,31 @@ type PauseProps = {
   signup?: string;
 };
 
+interface Keyboard {
+  getLayoutMap: () => any;
+}
+
+interface KeyboardLayoutMap {
+  get: (key: string) => any;
+}
+interface Navigator {
+  keyboard: Keyboard;
+}
+
 export default function DesktopPause(props: PauseProps) {
   const { dev, signup } = props;
   const { paused, overlay, setPaused, menuItems } = useEnvironment();
+  const controls = useRef("W/A/S/D");
   const closeOverlay = () => setPaused(false);
+
+  if (navigator.keyboard) {
+    const keyboard = navigator.keyboard;
+    keyboard.getLayoutMap().then((keyboardLayoutMap: KeyboardLayoutMap) => {
+      const upKey = keyboardLayoutMap.get("KeyW");
+      if (upKey === "z") controls.current = "Z/Q/S/D";
+      else controls.current = "W/A/S/D";
+    });
+  }
 
   if (dev) {
     return (
@@ -199,7 +221,7 @@ export default function DesktopPause(props: PauseProps) {
           <Title>muse</Title>
         </Header>
         <Text>
-          <p>Move around: {isMobile ? "Joystick" : "W/A/S/D"}</p>
+          <p>Move around: {isMobile ? "Joystick" : controls.current}</p>
           <p>Look around: {isMobile ? "Drag" : "Mouse"}</p>
           <p>Pause: {isMobile ? "Menu Button" : "Esc"}</p>
         </Text>
