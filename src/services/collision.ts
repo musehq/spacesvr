dd99654843ef7a41c9a4985ba5803f15ff475b55
Alex Shortt <alexander.shortt@gmail.com>
2021-06-08 01:55:29 -0700

    account for interleaved buffer attributes in trimesh collision hook

--- src/services/collision.ts
+++ src/services/collision.ts
@@ -1,10 +1,30 @@
-import { BufferGeometry } from "three";
+import {
+  BufferAttribute,
+  BufferGeometry,
+  InterleavedBufferAttribute,
+} from "three";
 import { useTrimesh } from "@react-three/cannon";
 
 export const useTrimeshCollision = (geometry: BufferGeometry) => {
-  const vertices = geometry.attributes.position.array;
-  // @ts-ignore
-  const indices = geometry.index.array;
+  const indices = (geometry.index as BufferAttribute).array;
+
+  const isInterleaved =
+    // @ts-ignore
+    geometry.attributes.position.isInterleavedBufferAttribute;
+
+  let vertices: number[] = [];
+  if (isInterleaved) {
+    const attr = geometry.attributes.position as InterleavedBufferAttribute;
+    const data = attr.data;
+    for (let i = attr.offset; i < data.array.length; i += data.stride) {
+      for (let x = 0; x < attr.itemSize; x++) {
+        vertices.push(data.array[i + x]);
+      }
+    }
+  } else {
+    vertices = (geometry.attributes.position as BufferAttribute)
+      .array as number[];
+  }
 
   // @ts-ignore
   const [hitbox] = useTrimesh(() => ({
