19d5877e0e3ff5a24794145c5d5ccea239276f76
Alex Shortt <alexander.shortt@gmail.com>
2021-07-21 17:26:16 -0700

    v1.7.1 add culori, remove shopify-buy

--- src/core/overlays/PauseMenu.tsx
+++ src/core/overlays/PauseMenu.tsx
@@ -186,7 +186,7 @@ export default function PauseMenu(props: PauseMenuProps) {
       )}`,
     },
     {
-      text: "v1.7.0",
+      text: "v1.7.1",
       action: () => {
         console.log("");
       },
