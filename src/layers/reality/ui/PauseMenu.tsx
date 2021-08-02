75e1ca12d158eb16a939746ebd1e0d15cbe7bcdb
Alex Shortt <alexander.shortt@gmail.com>
2021-08-01 19:38:02 -0700

    v1.7.6

--- src/layers/reality/ui/PauseMenu.tsx
+++ src/layers/reality/ui/PauseMenu.tsx
@@ -186,7 +186,7 @@ export default function PauseMenu(props: PauseMenuProps) {
       )}`,
     },
     {
-      text: "v1.7.5",
+      text: "v1.7.6",
       action: () => {
         console.log("");
       },
