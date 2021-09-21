ad7ba1ed833fbb38d334401f36f1645caa2f83cd
Alex Shortt <alexander.shortt@gmail.com>
2021-09-20 19:51:09 -0700

    freeze player until first press of continue

--- src/layers/reality/players/colliders/CapsuleCollider.tsx
+++ src/layers/reality/players/colliders/CapsuleCollider.tsx
@@ -1,5 +1,6 @@
 import { ShapeType, Triplet, useCompoundBody } from "@react-three/cannon";
-import { Vector3 } from "three";
+import { useEffect, useState } from "react";
+import { useEnvironment } from "../../layers/environment";
 
 // height of 0.9 (eye level) for a perceived height of 1
 const HEIGHT = 0.9;
@@ -15,14 +16,27 @@ const sphere3 = { ...sphereProps, position: [0, -RADIUS, 0] };
 export const useCapsuleCollider = (pos = [0, 0, 0]) => {
   const vPos = pos as Triplet;
 
-  const compoundBody = useCompoundBody(() => ({
-    mass: 62,
-    position: vPos,
-    segments: 8,
-    fixedRotation: true,
-    type: "Dynamic",
-    shapes: [sphere1, sphere2, sphere3],
-  }));
+  const { paused } = useEnvironment();
+  const [mass, setMass] = useState(0);
+
+  const compoundBody = useCompoundBody(
+    () => ({
+      mass: mass,
+      position: vPos,
+      segments: 8,
+      fixedRotation: true,
+      type: "Dynamic",
+      shapes: [sphere1, sphere2, sphere3],
+    }),
+    undefined,
+    [mass]
+  );
+
+  useEffect(() => {
+    if (!paused && mass === 0) {
+      setMass(62);
+    }
+  }, [mass, paused]);
 
   return compoundBody;
 };
