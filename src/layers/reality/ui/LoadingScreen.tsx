12f9ad6929d6f99a229eeb994eb8387b0f51e520
benjaminha14 <benjaminha14@gmail.com>
2021-11-22 20:30:02 -0800

    center mort, add floading text

--- src/layers/reality/ui/LoadingScreen.tsx
+++ src/layers/reality/ui/LoadingScreen.tsx
@@ -1,8 +1,22 @@
 import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
 import { useControlledProgress } from "../utils/loading";
-const LoadingVideo =
-  "https://d27rt3a60hh1lx.cloudfront.net/videos/mortloading.mp4";
 
+const LOADING_VIDEO =
+  "https://d27rt3a60hh1lx.cloudfront.net/videos/mortloading.mp4";
+const float = keyframes`
+  0% {
+    transform: translatey(0px);
+  }
+
+  50% {
+    transform: translatey(-15px);
+  }
+
+  100% {
+    transform: translatey(0px);
+  }
+`;
 const Container = styled.div<{ finished: boolean }>`
   width: 100%;
   height: 100%;
@@ -20,23 +34,22 @@ const Container = styled.div<{ finished: boolean }>`
   align-items: center;
   flex-direction: column;
 `;
-const VideoStyle = {
-  display: "block",
-  margin: "0 auto",
-};
+
+const Text = styled.div`
+  animation: ${float} 7s ease-in-out infinite;
+`;
+const Video = styled.video`
+  display: block;
+  margin: 0 auto;
+  z-index: -1;
+  position: absolute;
+`;
 export default function LoadingScreen() {
   const progress = useControlledProgress();
-
   return (
-    <Container finished={progress === 101}>
-      {Math.round(progress)}%
-      <video
-        style={VideoStyle}
-        autoPlay={true}
-        muted={true}
-        loop={true}
-        src={LoadingVideo}
-      />
+    <Container finished={progress === 100}>
+      <Text>{Math.round(progress)}% </Text>
+      <Video autoPlay muted loop src={LOADING_VIDEO} playsInline />
     </Container>
   );
 }
