5a44ee7c4a6d7ae0a114502a3b3a4b17cce9e66c
Alex Shortt <alexander.shortt@gmail.com>
2022-03-23 14:22:34 -0700

    v1.9.0

--- src/layers/reality/ui/PauseMenu/index.tsx
+++ src/layers/reality/ui/PauseMenu/index.tsx
@@ -53,7 +53,7 @@ export default function PauseMenu(props: PauseMenuProps) {
   const PAUSE_ITEMS: PauseItem[] = [
     ...pauseMenuItems,
     {
-      text: "v1.8.12",
+      text: "v1.9.0",
       link: "https://www.npmjs.com/package/spacesvr",
     },
     ...menuItems,
