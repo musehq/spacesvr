7850ea006515c031f2bd4867591a02879fb264c4
Alex Shortt <alexander.shortt@gmail.com>
2022-06-14 01:07:34 -0700

    Network Improvements (#90)
    * fix signalling route, fix multiplayer example, lower networked entity meshes
    
    * add snapshot interpolation, reduce framerate to 10fps
    
    * change multiplayer example to pr56 so we can test
    
    * fix backend route
    
    * don't cut off flot precision, sent quat instead of euler, bump fps to 12, move render updater, change back multiplayer example
    
    * add script to only print connection status on changes (not first time)

--- src/layers/Network/ideas/NetworkedEntities.tsx
+++ src/layers/Network/ideas/NetworkedEntities.tsx
@@ -1,5 +1,4 @@
 import { useMemo, useRef, useState } from "react";
-import { useFrame } from "@react-three/fiber";
 import {
   CylinderBufferGeometry,
   InstancedMesh,
@@ -7,13 +6,19 @@ import {
   Object3D,
 } from "three";
 import { useNetwork } from "../logic/network";
-import { useLimitedFrame, useLimiter } from "../../../logic/limiter";
+import { useLimitedFrame } from "../../../logic/limiter";
+import { SnapshotInterpolation } from "@geckos.io/snapshot-interpolation";
+import {
+  Snapshot,
+  Entity as EntityState,
+  Quat,
+} from "@geckos.io/snapshot-interpolation/lib/types";
 
 export default function NetworkedEntities() {
   const { connections, connected, useChannel } = useNetwork();
 
   const mesh = useRef<InstancedMesh>(null);
-  const geo = useMemo(() => new CylinderBufferGeometry(0.3, 0.3, 1, 30), []);
+  const geo = useMemo(() => new CylinderBufferGeometry(0.3, 0.3, 1, 32), []);
   const mat = useMemo(() => new MeshNormalMaterial(), []);
   const obj = useMemo(() => {
     const o = new Object3D();
@@ -30,45 +35,72 @@ export default function NetworkedEntities() {
     if (!sameIds) setEntityIds(ids);
   });
 
+  const NETWORK_FPS = 12;
   type Entity = { pos: number[]; rot: number[] };
+  const SI = useMemo(() => new SnapshotInterpolation(NETWORK_FPS), []);
   const entityChannel = useChannel<Entity, { [key in string]: Entity }>(
     "player",
     "stream",
     (m, s) => {
       if (!m.conn) return;
       s[m.conn.peer] = m.data;
+
+      const state: EntityState[] = Object.keys(s).map((key) => ({
+        id: key,
+        x: s[key].pos[0],
+        y: s[key].pos[1],
+        z: s[key].pos[2],
+        q: {
+          x: s[key].rot[0],
+          y: s[key].rot[1],
+          z: s[key].rot[2],
+          w: s[key].rot[3],
+        },
+      }));
+
+      const snapshot: Snapshot = {
+        id: Math.random().toString(),
+        time: new Date().getTime(),
+        state,
+      };
+
+      SI.vault.add(snapshot);
     }
   );
 
   // send own player data
-  useLimitedFrame(15, ({ camera }) => {
+  useLimitedFrame(NETWORK_FPS, ({ camera }) => {
     if (!connected) return;
     entityChannel.send({
-      pos: camera.position.toArray().map((p) => parseFloat(p.toPrecision(3))),
-      rot: camera.rotation
-        .toArray()
-        .slice(0, 3)
-        .map((r) => parseFloat(r.toPrecision(3))),
+      pos: camera.position.toArray(),
+      rot: camera.quaternion.toArray(),
     });
   });
 
   // receive player data
-  useLimitedFrame(50, () => {
+  useLimitedFrame(55, () => {
     if (!mesh.current) return;
-    for (const id of Object.keys(entityChannel.state)) {
-      const index = entityIds.indexOf(id);
-      if (index < 0) return;
-      const { pos, rot } = entityChannel.state[id];
-      obj.position.fromArray(pos);
-      obj.rotation.fromArray(rot);
-      obj.updateMatrix();
-      mesh.current?.setMatrixAt(index, obj.matrix);
-    }
-  });
 
-  const renderLimiter = useLimiter(40);
-  useFrame(({ clock }) => {
-    if (!mesh.current || !renderLimiter.isReady(clock)) return;
+    const snapshot = SI.calcInterpolation("x y z q(quat)");
+    if (!snapshot) return;
+
+    let i = 0;
+    for (const entityState of snapshot.state) {
+      const { x, y, z, q } = entityState;
+      obj.position.x = x as number;
+      obj.position.y = y as number;
+      obj.position.z = z as number;
+      obj.position.y -= 0.2; // they were floating before, idk where the constant comes from really
+      const quat = q as Quat;
+      obj.quaternion.x = quat.x;
+      obj.quaternion.y = quat.y;
+      obj.quaternion.z = quat.z;
+      obj.quaternion.w = quat.w;
+      obj.updateMatrix();
+      mesh.current.setMatrixAt(i, obj.matrix);
+      i++;
+    }
+
     mesh.current.instanceMatrix.needsUpdate = true;
   });
 
