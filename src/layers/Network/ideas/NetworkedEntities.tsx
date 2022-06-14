a5d7a65100b170edd1729315e37d5acc71bc4e95
Alex Shortt <alexander.shortt@gmail.com>
2022-06-13 23:03:17 -0700

    add snapshot interpolation, reduce framerate to 10fps

--- src/layers/Network/ideas/NetworkedEntities.tsx
+++ src/layers/Network/ideas/NetworkedEntities.tsx
@@ -8,6 +8,11 @@ import {
 } from "three";
 import { useNetwork } from "../logic/network";
 import { useLimitedFrame, useLimiter } from "../../../logic/limiter";
+import { SnapshotInterpolation } from "@geckos.io/snapshot-interpolation";
+import {
+  Snapshot,
+  Entity as EntityState,
+} from "@geckos.io/snapshot-interpolation/lib/types";
 
 export default function NetworkedEntities() {
   const { connections, connected, useChannel } = useNetwork();
@@ -30,18 +35,38 @@ export default function NetworkedEntities() {
     if (!sameIds) setEntityIds(ids);
   });
 
+  const NETWORK_FPS = 10;
   type Entity = { pos: number[]; rot: number[] };
+  const SI = useMemo(() => new SnapshotInterpolation(NETWORK_FPS), []);
   const entityChannel = useChannel<Entity, { [key in string]: Entity }>(
     "player",
     "stream",
     (m, s) => {
       if (!m.conn) return;
       s[m.conn.peer] = m.data;
+
+      const state: EntityState[] = Object.keys(s).map((key) => ({
+        id: key,
+        x: s[key].pos[0],
+        y: s[key].pos[1],
+        z: s[key].pos[2],
+        rx: s[key].rot[0],
+        ry: s[key].rot[1],
+        rz: s[key].rot[2],
+      }));
+
+      const snapshot: Snapshot = {
+        id: Math.random().toString(),
+        time: new Date().getTime(),
+        state,
+      };
+
+      SI.vault.add(snapshot);
     }
   );
 
   // send own player data
-  useLimitedFrame(15, ({ camera }) => {
+  useLimitedFrame(NETWORK_FPS, ({ camera }) => {
     if (!connected) return;
     entityChannel.send({
       pos: camera.position.toArray().map((p) => parseFloat(p.toPrecision(3))),
@@ -53,17 +78,25 @@ export default function NetworkedEntities() {
   });
 
   // receive player data
-  useLimitedFrame(50, () => {
+  useLimitedFrame(55, () => {
     if (!mesh.current) return;
-    for (const id of Object.keys(entityChannel.state)) {
-      const index = entityIds.indexOf(id);
-      if (index < 0) return;
-      const { pos, rot } = entityChannel.state[id];
-      obj.position.fromArray(pos);
+
+    const snapshot = SI.calcInterpolation("x y z rx ry rz");
+    if (!snapshot) return;
+
+    let i = 0;
+    for (const entityState of snapshot.state) {
+      const { x, y, z, rx, ry, rz } = entityState;
+      obj.position.x = x as number;
+      obj.position.y = y as number;
+      obj.position.z = z as number;
       obj.position.y -= 0.2; // they were floating before, idk where the constant comes from really
-      obj.rotation.fromArray(rot);
+      obj.rotation.x = rx as number;
+      obj.rotation.y = ry as number;
+      obj.rotation.z = rz as number;
       obj.updateMatrix();
-      mesh.current?.setMatrixAt(index, obj.matrix);
+      mesh.current.setMatrixAt(i, obj.matrix);
+      i++;
     }
   });
 
