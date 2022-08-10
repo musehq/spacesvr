2bc7214524531751ef5c10af26079f64c81a7a96
Alex Shortt <alexander.shortt@gmail.com>
2022-08-09 19:00:31 -0700

    add volume controls

--- src/layers/Environment/tools/Config.tsx
+++ src/layers/Environment/tools/Config.tsx
@@ -7,30 +7,30 @@ export default function Config() {
   const [muted, setMuted] = useState(false);
   const scene = useThree((st) => st.scene);
 
-  useEffect(() => {
-    if (muted) {
-      scene.traverse((child) => {
-        if (child instanceof AudioListener) {
-          (child as AudioListener).setMasterVolume(0);
-        }
-      });
-    } else {
-      scene.traverse((child) => {
-        if (child instanceof AudioListener) {
-          (child as AudioListener).setMasterVolume(1);
-        }
-      });
-    }
-  }, [muted, scene]);
+  const [volume, setVolume] = useState(1);
+
+  const setVol = (vol: number) => {
+    scene.traverse((child) => {
+      if (child instanceof AudioListener) {
+        (child as AudioListener).setMasterVolume(vol);
+      }
+    });
+  };
 
   useEffect(() => {
-    const toggle = (e: KeyboardEvent) => {
-      if (e.key.toLowerCase() !== "m") return;
-      setMuted(!muted);
+    if (muted) setVol(0);
+    else setVol(volume);
+  }, [muted, scene, setVol, volume]);
+
+  useEffect(() => {
+    const ev = (e: KeyboardEvent) => {
+      if (e.key.toLowerCase() === "m") setMuted(!muted);
+      if (e.code === "ArrowDown") setVolume(Math.max(0, volume - 0.1));
+      if (e.code === "ArrowUp") setVolume(Math.min(1, volume + 0.1));
     };
-    document.addEventListener("keydown", toggle);
-    return () => document.removeEventListener("keydown", toggle);
-  }, [muted]);
+    document.addEventListener("keydown", ev);
+    return () => document.removeEventListener("keydown", ev);
+  }, [muted, volume]);
 
   return (
     <Tool face pos={[0.65, -0.75]}>
