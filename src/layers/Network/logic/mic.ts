c526ac450aada28905c91385c9839191a63ded25
Alex Shortt <alexander.shortt@gmail.com>
2022-09-07 01:11:18 +0200

    attempt a mic fix

--- src/layers/Network/logic/mic.ts
+++ src/layers/Network/logic/mic.ts
@@ -1,16 +1,37 @@
 import { useEffect, useState } from "react";
+import { useEnvironment } from "../../Environment";
 
 /**
  * WHen enabled, will ask user for mic permissions and return the local microphone stream
  * @param enabled
  */
 export const useMicrophone = (enabled = true): MediaStream | undefined => {
+  const { paused } = useEnvironment();
+  const [firstPaused, setFirstPaused] = useState(true);
+  useEffect(() => setFirstPaused(paused && firstPaused), [paused, firstPaused]);
+
+  function iOS() {
+    return (
+      [
+        "iPad Simulator",
+        "iPhone Simulator",
+        "iPod Simulator",
+        "iPad",
+        "iPhone",
+        "iPod",
+      ].includes(navigator.platform) ||
+      // iPad on iOS 13 detection
+      (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+    );
+  }
+
   const [localStream, setLocalStream] = useState<MediaStream>();
 
   // attempt to request permission for microphone, only try once
   const [attempted, setAttempted] = useState(false);
   useEffect(() => {
-    if (!enabled || attempted) return;
+    // https://bugs.webkit.org/show_bug.cgi?id=230902#c47
+    if (!enabled || attempted || (iOS() && firstPaused)) return;
 
     setAttempted(true);
 
@@ -27,7 +48,7 @@ export const useMicrophone = (enabled = true): MediaStream | undefined => {
         console.error(err);
       }
     );
-  }, [attempted, enabled]);
+  }, [attempted, enabled, firstPaused]);
 
   return localStream;
 };
