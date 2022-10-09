56b527c8f30a6aad87adddc993ab89284285667a
Alex Shortt <alexander.shortt@gmail.com>
2022-10-08 20:11:25 -0700

    clone gltf in model idea, add normalize prop

--- src/ideas/media/Model.tsx
+++ src/ideas/media/Model.tsx
@@ -1,24 +1,38 @@
-import { Fragment, Suspense } from "react";
+import { Suspense, useMemo } from "react";
 import { GroupProps } from "@react-three/fiber";
-import { Center } from "@react-three/drei";
 import { useModel } from "../../logic";
+import { Box3, Vector3 } from "three";
+import { SkeletonUtils } from "three-stdlib";
 
 type ModelProps = {
   src: string;
   center?: boolean;
+  normalize?: boolean;
 } & GroupProps;
 
 function UnsuspensedModel(props: ModelProps) {
-  const { src, center, ...rest } = props;
-  const gltf = useModel(src);
+  const { src, center, normalize, ...rest } = props;
 
-  const Parent = center ? Center : Fragment;
+  const gltf = useModel(src);
+  const model = useMemo(() => SkeletonUtils.clone(gltf.scene), [gltf.scene]);
+
+  const bbox = useMemo(() => new Box3().setFromObject(model), [model]);
+  const centerVec = useMemo(
+    () => bbox.getCenter(new Vector3()).multiplyScalar(-1),
+    [bbox]
+  );
+
+  const sizeX = bbox.max.x - bbox.min.x;
+  const sizeY = bbox.max.y - bbox.min.y;
+  const sizeZ = bbox.max.z - bbox.min.z;
+  const maxSide = Math.max(sizeX, sizeY, sizeZ);
+  const NORM_SCALE = 1 / maxSide;
 
   return (
     <group name="spacesvr-model" {...rest}>
-      <Parent>
-        <primitive object={gltf.scene} />
-      </Parent>
+      <group scale={normalize ? NORM_SCALE : 1}>
+        <primitive object={model} position={center ? centerVec : undefined} />
+      </group>
     </group>
   );
 }
