fe2f5abd4c4089eb334ef37fd89b3234f4ebb090
Alex Shortt <alexander.shortt@gmail.com>
2022-10-19 11:05:24 -0700

    add favicon and seo, improve hub world and workshop world design

--- examples/worlds/Hub.tsx
+++ examples/worlds/Hub.tsx
@@ -9,22 +9,26 @@ export default function Hub() {
     >
       <LostWorld />
       <group position-z={-1.5}>
-        <Title position-y={1.2}>welcome to spacesvr</Title>
+        <Title
+          position-y={1.2}
+          image="https://d27rt3a60hh1lx.cloudfront.net/spacesvr/spacesvr.png"
+        >
+          welcome to spacesvr
+        </Title>
         <group position-y={0.8}>
-          <Link href="/multiplayer" position-x={-1}>
-            test out multiplayer
+          <Link href="/multiplayer" position-x={-2}>
+            visit multiplayer page
           </Link>
-          <Link href="/media">test out media</Link>
+          <Link href="/media" position-x={-1}>
+            visit media page
+          </Link>
           <Link href="/workshop" position-x={1}>
-            visit the workshop
+            visit workshop page
+          </Link>
+          <Link href="https://github.com/musehq/spacesvr" position-x={2}>
+            visit github
           </Link>
         </group>
-        <Image
-          scale={18}
-          position-y={0.025}
-          rotation-x={-Math.PI / 2}
-          src="https://d27rt3a60hh1lx.cloudfront.net/spacesvr/spacesvr.png"
-        />
       </group>
     </StandardReality>
   );
