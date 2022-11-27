70f726ad173dad1805f7a1c7b2cd99028a72836b
Alex Shortt <alexander.shortt@gmail.com>
2022-11-26 23:57:24 -0800

    update player collider closer to old settings, update collidable modifier less often

--- src/ideas/modifiers/Collidable/components/TrimeshCollider.tsx
+++ src/ideas/modifiers/Collidable/components/TrimeshCollider.tsx
@@ -1,7 +1,13 @@
-import { BufferGeometry, Euler, Group, Quaternion, Vector3 } from "three";
+import {
+  BufferGeometry,
+  Euler,
+  Group,
+  Matrix4,
+  Quaternion,
+  Vector3,
+} from "three";
 import { useMemo, useRef, useState } from "react";
-import { useLimiter } from "../../../../logic/limiter";
-import { useFrame } from "@react-three/fiber";
+import { useLimitedFrame } from "../../../../logic/limiter";
 import { useTrimeshCollision } from "../utils/trimesh";
 
 type TrimeshColliderProps = {
@@ -13,33 +19,51 @@ export default function TrimeshCollider(props: TrimeshColliderProps) {
 
   const group = useRef<Group>(null);
 
-  const dummyPos = useMemo(() => new Vector3(), []);
-  const dummyQuat = useMemo(() => new Quaternion(), []);
-  const dummyEuler = useMemo(() => new Euler(), []);
-  const dummyScale = useMemo(() => new Vector3(1, 1, 1), []);
-  const [scale, setScale] = useState(new Vector3(1, 1, 1));
+  const [pos] = useState(() => new Vector3());
+  const [quat] = useState(() => new Quaternion());
+  const [scale] = useState(() => new Vector3());
+  const [euler] = useState(() => new Euler());
+  const [curScale, setCurScale] = useState(new Vector3(1, 1, 1));
 
   const geometry = useMemo(() => {
-    const g = geo.clone().scale(scale.x, scale.y, scale.z);
+    const g = geo.clone().scale(curScale.x, curScale.y, curScale.z);
     g.computeVertexNormals();
     return g;
-  }, [geo, scale]);
+  }, [geo, curScale]);
 
   const [, api] = useTrimeshCollision(geometry);
 
-  const limiter = useLimiter(5);
-  useFrame(({ clock }) => {
-    if (!limiter.isReady(clock) || !group.current) return;
+  // there's some state update that causes the api not to receive an out of sync position
+  // i think it's whenever the api gets recreated. for now, just re-apply transforms every 2 seconds
+  const needsUpdate = useRef(false);
+  useLimitedFrame(1 / 2, () => {
+    needsUpdate.current = true;
+  });
 
-    group.current.getWorldPosition(dummyPos);
-    group.current.getWorldQuaternion(dummyQuat);
-    group.current.getWorldScale(dummyScale);
+  const lastUpdatedMatrix = useRef<Matrix4>(new Matrix4());
+  useLimitedFrame(8, () => {
+    if (!group.current) return;
 
-    api.position.copy(dummyPos);
-    api.rotation.copy(dummyEuler.setFromQuaternion(dummyQuat));
-    if (!dummyScale.equals(scale)) {
-      setScale(dummyScale.clone());
+    // get global position, rotation, scale
+    group.current.updateWorldMatrix(true, false);
+    group.current.matrixWorld.decompose(pos, quat, scale);
+
+    // no need to update if nothing changed
+    if (
+      lastUpdatedMatrix.current.equals(group.current.matrixWorld) &&
+      !needsUpdate.current
+    ) {
+      return;
     }
+
+    // update last values
+    lastUpdatedMatrix.current.copy(group.current.matrixWorld);
+    needsUpdate.current = false;
+
+    // if a change was found, update collider
+    api.position.copy(pos);
+    api.rotation.copy(euler.setFromQuaternion(quat));
+    if (!scale.equals(curScale)) setCurScale(scale.clone());
   });
 
   return <group ref={group} />;
