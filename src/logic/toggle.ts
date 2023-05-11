dd8b4025ea3d4ff2b248f0dc9fcb25bd89d7b38e
Alex Shortt <alexander.shortt@gmail.com>
2023-05-11 16:24:32 -0700

    fix timeout code

--- src/logic/toggle.ts
+++ src/logic/toggle.ts
@@ -1,25 +1,33 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import Timeout = NodeJS.Timeout;
 
 /**
  * A hook that allows you to toggle a boolean value.
  * @param delay time in ms
  */
 export const useDelayedToggle = (delay = 1000) => {
-  const lastTimeSet = useRef(0);
+  const timeoutId = useRef<Timeout | null>(null);
   const [active, setActiveState] = useState(false);
 
-  if (active && Date.now() - lastTimeSet.current > delay) {
-    lastTimeSet.current = Date.now();
-    setActiveState(!active);
-  }
-
   const setActive = useCallback(() => {
+    if (timeoutId.current !== null) {
+      clearTimeout(timeoutId.current);
+    }
+
     setActiveState(true);
-    lastTimeSet.current = Date.now();
-    setTimeout(() => {
-      if (Date.now() - lastTimeSet.current > delay) setActiveState(false);
+
+    timeoutId.current = setTimeout(() => {
+      setActiveState(false);
     }, delay);
   }, [delay]);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutId.current !== null) {
+        clearTimeout(timeoutId.current);
+      }
+    };
+  }, []);
 
   return { active, setActive };
 };
